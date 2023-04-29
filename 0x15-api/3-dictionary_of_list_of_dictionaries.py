d17242ef8e1821cc8b7c296cca5b81a6f7c78b01
Anionovo Ugochukwu E <esuneteng.tech@gmail.com>
2023-04-29 16:47:20 +0100

    Create 3-dictionary_of_list_of_dictionaries.py

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat April 29 16:46:14 2023
+@author: Anionovo Ugochukwu 
+"""
+from json import dump
+from requests import get
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    response = get(url)
+    users = response.json()
+
+    dictionary = {}
+    for user in users:
+        user_id = user.get('id')
+        username = user.get('username')
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+        url = url + '/todos/'
+        response = get(url)
+        tasks = response.json()
+        dictionary[user_id] = []
+        for task in tasks:
+            dictionary[user_id].append({
+                                        "task": task.get('title'),
+                                        "completed": task.get('completed'),
+                                        "username": username
+                                        })
+    with open('todo_all_employees.json', 'w') as file:
+        dump(dictionary, file)
